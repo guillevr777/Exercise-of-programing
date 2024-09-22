413c7d1020931d6a29b50febfcb89743edb30c73
guill <guill@DESKTOP-0V4E8HS>
2024-09-22 23:07:50 +0200

    mejor

--- Exercise1/src/exercise1/exercise1.java
+++ Exercise1/src/exercise1/exercise1.java
@@ -3,10 +3,10 @@ package exercise1;
 public class exercise1 {
 	public static void main(String[] args) {
 		System.out.println("**********	 *********");
-		System.out.println("*			 *		  ");
-		System.out.println("*			 *        ");
-		System.out.println("*	 *****	 *********");
-		System.out.println("*		 *	  		 *");
+		System.out.println("*	         *");
+		System.out.println("*	         *");
+		System.out.println("*    *****	 *********");
+		System.out.println("*        *	         *");
 		System.out.println("*        *	         *");
 		System.out.println("**********	 *********");
 	}
